dc56aec12eee130b7ddd60bd674a4fed7d003165
Will Farris <9190155+WillFarris@users.noreply.github.com>
2020-03-05 17:45:49 -0800

    Cleaned up imports

--- src/org/farriswheel/Client.java
+++ src/org/farriswheel/Client.java
@@ -56,6 +56,8 @@ public class Client implements Runnable {
             if(message == null)
                 break;
             switch (message) {
+                case ACK:
+                    break;
                 case YOURMOVE:
                     yourmove = true;
                     break;
--- src/org/farriswheel/ServerGameSession.java
+++ src/org/farriswheel/ServerGameSession.java
@@ -5,10 +5,13 @@ import java.net.Socket;
 
 public class ServerGameSession implements Runnable {
 
-    public static final int BUFFERSIZE = 32;
+    public static final int BUFFERSIZE = 16;
 
+    public static final String SETUPINFO   = "SETUPINFO";  //request nickname
     public static final String YOURMOVE  = "YOURMOVE";
-    public static final String OTHERMOVE = "OTHERMOVE";
+    public static final String OTHERMOVE = "OTHERMOV";
+    public static final String MYMOVE    = "MYMOVE";
+    public static final String BADMOVE   = "BADMOVE";
     public static final String ACK       = "ACK";
     public static final String YOUWIN    = "YOUWIN";
     public static final String YOULOSE   = "YOULOSE";
@@ -37,6 +40,7 @@ public class ServerGameSession implements Runnable {
     @Override
     public void run() {
         try {
+            player0.writeLine("SETUPINFO");
             player0.readPlayerInfo('X');
             player1.readPlayerInfo('O');
             gameLoop();
@@ -51,7 +55,31 @@ public class ServerGameSession implements Runnable {
     private void gameLoop() throws IOException {
 
         do {
+
             player0.writeLine(YOURMOVE);
+            String player0Response = player0.readLine();
+            player0.writeLine(ACK);
+            int win = 0;
+            switch (player0Response) {
+                case MYMOVE:
+                    while (checkWin(player0.readLine(), player0.getSymbol()) == -1) {
+                        player0.writeLine(BADMOVE);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            player1.writeLine(YOURMOVE);
+            String player1Response = player1.readLine();
+            player1.writeLine(ACK);
+            win = 0;
+            switch (player1Response) {
+                default:
+                    break;
+            }
+
+            /*player0.writeLine(YOURMOVE);
             String p0Move = player0.readLine();
             if (checkWin(p0Move, player0.getSymbol())) {
                 player0.writeLine(YOUWIN);
@@ -75,35 +103,43 @@ public class ServerGameSession implements Runnable {
             player0.writeLine(OTHERMOVE);
             System.out.println("[SERVER] P0: "+player0.readLine());
             player0.writeLine(p1Move);
-            System.out.println("[SERVER] P0: "+player0.readLine());
+            System.out.println("[SERVER] P0: "+player0.readLine());*/
 
 
         } while (player0.isConnected() && player1.isConnected());
         System.out.println("Server terminating game loop");
     }
 
-    private boolean checkWin(String move, char symbol) {
+    /* checks board for win
+     * Return codes:
+     * 1  -> Game was won with move
+     * 0  -> Game was not won with move
+     * -1 -> The chosen spot has already been picked
+     */
+    private int checkWin(String move, char symbol) {
         String [] coords = move.split(":");
         int newX = Integer.valueOf(coords[0]);
         int newY = Integer.valueOf(coords[1]);
+        if(board[newX][newY] != ' ')
+            return -1;
         board[newX][newY] = symbol;
 
         for(int rc=0;rc<3;++rc) {
             if(board[rc][0] != ' ' && board[rc][0] == board[rc][1] && board[rc][1] == board[rc][2]) {
-                return  true;
+                return  1;
             }
             if(board[0][rc] != ' ' && board[0][rc] == board[1][rc] && board[1][rc] == board[2][rc]) {
-                return  true;
+                return  1;
             }
         }
         if(board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
-            return true;
+            return 1;
 
         if(board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0])
-            return true;
+            return 1;
 
 
-        return false;
+        return 0;
     }
 
 
